ce887c7867a39fb87beaeb8f770ad11beae3b440
Ngoc Tu NGUYEN <nguyenngoct2112@gmail.com>
2024-12-01 20:41:55 +0100

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -19,22 +19,88 @@ declare module "vue3-popper" {
   | "left-end";
 
   export interface PopperProps {
+    /**
+     * Preferred placement of the Popper
+     * @default bottom
+    */
     placement?: Placement;
+    /**
+     * Disables automatically closing the Popper when the user clicks away from it
+     * @default false
+     */
     disableClickAway?: boolean;
+    /**
+     * Offset in pixels along the trigger element
+     * @default '0'
+     */
     offsetSkid?: string;
+    /**
+     * Offset in pixels away from the trigger element
+     * @default '12'
+     */
     offsetDistance?: string;
+    /**
+     * Trigger the Popper on hover
+     * @default false
+     */
     hover?: boolean;
+    /**
+     * Control the Popper manually, other events (click, hover) are ignored if this is set to true/false
+     * @default null
+     */
     show?: boolean;
+    /**
+     * Disables the Popper. If it was already open, it will be closed.
+     * @default false
+     */
     disabled?: boolean;
+    /**
+     * Open the Popper after a delay (ms)
+     * @default 0
+     */
     openDelay?: number | string;
+    /**
+     * Close the Popper after a delay (ms)
+     * @default 0
+     */
     closeDelay?: number | string;
+    /**
+     * The z-index of the Popper
+     * @default 9999
+     */
     zIndex?: number | string;
+    /**
+     * Display an arrow on the Popper
+     * @default false
+     */
     arrow?: boolean;
+    /**
+     * Stop arrow from reaching the edge of the Popper (in pixels)
+     * @default '0'
+     */
     arrowPadding?: string;
+    /**
+     * If the Popper should be interactive, it will close when clicked/hovered if false
+     * @default true
+     */
     interactive?: boolean;
+    /**
+     * If the Popper should be interactive, it will close when clicked/hovered if false
+     * @default true
+     */
     locked?: boolean;
+    /**
+     * If your content is just a simple string, you can pass it as a prop
+     * @default null
+     */
     content?: string;
+    /**
+     * When the Popper is opened
+     */
     "onOpen:popper"?: () => void;
+    /**
+     * When the Popper is hidden
+     */
     "onClose:popper"?: () => void;
   }
 
